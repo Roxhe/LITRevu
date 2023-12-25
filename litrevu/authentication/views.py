4f50c7ac6774a1d25d02716688a16c633cf38a26
Léo Da Rocha <leo.da.rocha2002@gmail.com>
2023-12-25 17:53:28 +0100

    add signup features

--- litrevu/authentication/views.py
+++ litrevu/authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
+from django.conf import settings
 
 from . import forms
 
@@ -22,7 +23,19 @@ def login_page(request):
     return render(
         request, 'authentication/login.html', context={'form': form, 'message': message})
 
+
 def logout_user(request):
 
     logout(request)
     return redirect('login')
+
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})
--- litrevu/litrevu/settings.py
+++ litrevu/litrevu/settings.py
@@ -90,16 +90,13 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'authentication.validators.ContainsLetterValidator',
     },
 ]
 
@@ -107,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
