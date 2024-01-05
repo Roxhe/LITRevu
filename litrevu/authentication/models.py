54411fed7af5d17838cb61e9a10ab4058943a0e1
Léo Da Rocha <leo.da.rocha2002@gmail.com>
2024-01-05 18:11:29 +0100

    work on models

--- litrevu/authentication/models.py
+++ litrevu/authentication/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    follows = models.ManyToManyField('self', symmetrical=False, verbose_name='suit')
 
     def __str__(self):
         return self.email
--- litrevu/blog/models.py
+++ litrevu/blog/models.py
@@ -1,3 +1,21 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=1024)
+    #photo =
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    #pined_review =
+
+class Review(models.Model):
+    title = models.CharField(max_length=128)
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    description = models.CharField(max_length=4096, blank=True)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    #is_pined =
